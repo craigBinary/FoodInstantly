32328c9de039f63a54fc52c1bad85b5ee9eb47b6
CraigFernández <cfcraig64@gmail.com>
2017-05-31 16:54:45 -0400

    Listar select
    lista de datos para select principal de busqueda

---
+++ Cliente/inc/selectComuna.php
@@ -0,0 +1,8 @@
+<?php
+include('claseCliente.php');
+$obj = new claseCliente();
+//$comunas= array();
+ echo $obj->listarComunas()  ;
+// print_r($obj->listarRestaurant())  ;
+
+ ?>
---
+++ Cliente/inc/selectRestaurant.php
@@ -0,0 +1,8 @@
+<?php
+include('claseCliente.php');
+$obj = new claseCliente();
+//$comunas= array();
+ echo $obj->listarRestaurant()  ;
+// print_r($obj->listarRestaurant())  ;
+
+ ?>
