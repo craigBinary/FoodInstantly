8a82c2e2f2fcda11d3e4ee202fe959944ed7ed81
CraigFernández <cfcraig64@gmail.com>
2017-06-20 03:11:08 -0400

    cambio chico
    se agregó el link de home y se acomodó bien el responsive y otras
    cosillas

--- Cliente/productos.php
+++ Cliente/productos.php
@@ -47,7 +47,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	<!-- breadcrumb -->  
 	<div class="container">	
 		<ol class="breadcrumb w3l-crumbs">
-			<li><a href="#"><i class="fa fa-home"></i> Home</a></li> 
+			<li><a href="index.php"><i class="fa fa-home"></i> Home</a></li> 
 			<li class="active">Productos</li>
 		</ol>
 	</div>
@@ -55,24 +55,21 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	<!-- products -->
 	<div class="products">	 
 		<div class="container">
-			<div class="col-md-9 product-w3ls-right"> 
+			<div class="col-md-12 product-w3ls-right"> 
 				<div class="product-top">
-					<h4>Food Collection</h4>
+					<h4>Hazla Corta</h4>
 					<ul> 
 						<li class="dropdown head-dpdn">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Filter By<span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tipo de comida<span class="caret"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="#">Low price</a></li> 
-								<li><a href="#">High price</a></li>
-								<li><a href="#">Latest</a></li>  
-							</ul> 
-						</li>
-						<li class="dropdown head-dpdn">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Food Type<span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Breakfast</a></li> 
-								<li><a href="#">Lunch</a></li>
-								<li><a href="#">Dinner</a></li>  
+								<li><a href="productos.php?id_restaurant=<?php echo $_GET['id_restaurant']; ?>">TODOS</a></li>
+								<?php
+								include ('inc/claseCliente.php'); 
+								$lista= new claseCliente();
+								$id_restaurant = $_GET['id_restaurant'];
+								$lista->listarTipoPlato($id_restaurant);
+								
+								?>
 							</ul> 
 						</li>
 					</ul> 
@@ -80,120 +77,17 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				</div>
 				<div class="products-row">
 
-				<?php	
-					include ('inc/claseCliente.php'); 
-					$lista= new claseCliente();
-					$id_restaurant = $_POST['id_restaurant'];
-					$lista->listarPlatos($id_restaurant);
-
+				<?php
+					if(!isset($_GET['id_tipo'])){
+						$lista->listarPlatos($id_restaurant);
+					}else{
+						
+						$id_tipo=$_GET['id_tipo'];
+						$lista->listarPlatos2($id_restaurant,$id_tipo);
+					}
 
 				?>
 					<div class="clearfix"> </div>
-				</div>
-			</div>
-			
-			<div class="col-md-3 rsidebar">
-				<div class="rsidebar-top">
-					<div class="slider-left">
-						<h4>CHOOSE BY AGE</h4>            
-						<div class="row row1 scroll-pane">
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Kids</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Youngers</label>  
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Adults</label> 
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Senior Citizen</label>  
-						</div> 
-					</div>
-					<div class="sidebar-row">
-						<h4>By Cuisine</h4>
-						<ul class="faq">
-							<li class="item1"><a href="#">Indian Food<span class="glyphicon glyphicon-menu-down"></span></a>
-								<ul>
-									<li class="subitem1"><a href="#">Breakfast</a></li>										
-									<li class="subitem1"><a href="#">Lunch</a></li>										
-									<li class="subitem1"><a href="#">Dinner</a></li>										
-									<li class="subitem1"><a href="#">Trending Food</a></li>										
-								</ul>
-							</li>
-							<li class="item2"><a href="#">Italian Food<span class="glyphicon glyphicon-menu-down"></span></a>
-								<ul>
-									<li class="subitem1"><a href="#">Panzanella</a></li>										
-									<li class="subitem1"><a href="#">Margherita Pizza </a></li>										
-									<li class="subitem1"><a href="#">Panna Cotta</a></li>										
-									<li class="subitem1"><a href="#">Corn Cannelloni</a></li>										
-								</ul>
-							</li>
-							<li class="item3"><a href="#">American Food<span class="glyphicon glyphicon-menu-down"></span></a>
-								<ul>
-									<li class="subitem1"><a href="#">Sandwiches</a></li>										
-									<li class="subitem1"><a href="#">Hot dog</a></li>										
-									<li class="subitem1"><a href="#">Chicken bog</a></li>										
-								</ul>
-							</li>
-						</ul>
-						<div class="clearfix"> </div> 
-						<!-- script for tabs -->
-						<script type="text/javascript">
-							$(function() {
-							
-								var menu_ul = $('.faq > li > ul'),
-									   menu_a  = $('.faq > li > a');
-								
-								menu_ul.hide();
-							
-								menu_a.click(function(e) {
-									e.preventDefault();
-									if(!$(this).hasClass('active')) {
-										menu_a.removeClass('active');
-										menu_ul.filter(':visible').slideUp('normal');
-										$(this).addClass('active').next().stop(true,true).slideDown('normal');
-									} else {
-										$(this).removeClass('active');
-										$(this).next().stop(true,true).slideUp('normal');
-									}
-								});
-							
-							});
-						</script>
-						<!-- script for tabs -->
-					</div>
-					<div class="sidebar-row">
-						<h4>DISCOUNTS</h4>
-						<div class="row row1 scroll-pane">  
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>30% - 20% (2)</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>10% - 5% (5)</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>30% - 20% (7)</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>10% - 5% (2)</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Other(50)</label>
-						</div>
-					</div>
-					<div class="sidebar-row">
-						<h4>DELIVERY TIME</h4>
-						<div class="row row1 scroll-pane">
-							<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i></i>6.30 am to 8.30 am</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>11.30 am to 1.30 pm</label>
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>7.30 pm to 10.00 pm</label> 
-						</div>
-					</div>			 
-				</div>
-				<div class="related-row">
-					<h4>Related Searches</h4>
-					<ul>
-						<li><a href="products.html">Salads </a></li>
-						<li><a href="products.html">Vegetarian</a></li>
-						<li><a href="products.html">Dinner</a></li>
-						<li><a href="products.html">Diet Soup</a></li>
-						<li><a href="products.html">Sweets</a></li>
-						<li><a href="products.html">Seasonal</a></li>
-						<li><a href="products.html">Breakfast</a></li>
-						<li><a href="products.html">Italian Food</a></li>
-						<li><a href="products.html">Meals</a></li> 
-					</ul>
-				</div>
-				<div class="related-row">
-					<h4>YOU MAY ALSO LIKE</h4>
-					<div class="galry-like">  
-						<a href="#" data-toggle="modal" data-target="#myModal1"><img src="images/s1.jpg" class="img-responsive" alt="img"></a>         
-					</div>
 				</div>
 			</div>
 			<div class="clearfix"> </div> 
@@ -285,15 +179,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				</div>
 				<section>
           		<div class="modal-body">
-
+				<!-- aquí va la llamada de la vista previa  -->
           		</div>
-          		</section>
-				<!-- aquí va la llamada de la seccion  -->
-				<!-- $id_restaurant; pasarle el valor con jquery, falta id de plato  ,$id_plato-->
-				<?php
-				 
-					//$lista-> previaRestorant($id_restaurant,$id_plato);
-				 ?> 
+          		</section> 
 			</div>
 		</div>
 	</div> 
@@ -423,6 +311,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.js"></script>
-    <script type="text/javascript" src="js/ajax_previa.js"></script>
+    <script type="text/javascript" src="js/ajax_previa.js"></script>   
 </body>
 </html>
