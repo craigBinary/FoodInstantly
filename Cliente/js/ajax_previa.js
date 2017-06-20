aa1c8421647a2f8bdae4d1aca50a367b07d65822
CraigFernández <cfcraig64@gmail.com>
2017-06-20 03:11:38 -0400

    comentario
    se comento funcion

--- Cliente/js/ajax_previa.js
+++ Cliente/js/ajax_previa.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
+	/*
+ $("a.tipo_plato").click(function(){
+	   var id_restaurant=document.getElementById("id_restaurant").value;		        	
+       var id_tipo = $(this).attr('id');
+       
+	     $.ajax({
+		type: 'POST',
+		url: 'inc/listarPlatos.php',
+		data: {'id_restaurant':id_restaurant ,'id_tipo': id_tipo}
+		
+	})
 	
+	.done(function(listas_rep){
+		$('.products-row').html(listas_rep)
+	})
+	.fail(function(){
+		alert('Hubo un problema ')
+	})
+	}) */
+
 	 $("a.buscame").click(function(){
-	var id_restaurant=document.getElementById("id_restaurant").value;		        	
-       var id = $(this).attr('id');
+		var id_restaurant=document.getElementById("id_restaurant").value;			        	
+        var id = $(this).attr('id');
        //alert(id_restaurant);
 	       //window.location.href = window.location.href + "?id_plato=" + id_plato;
 	     $.ajax({
@@ -19,4 +38,6 @@ $(document).ready(function(){
 		alert('Hubo un problema ')
 	})
 	})
+
+	 
 })
